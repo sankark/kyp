e7b1d9846283b964eb90d5069d80251060700aed
Sankar Kanagarathinam <sankark@users.noreply.github.com>
2015-12-30 11:34:48 -0600

    Update log.go

--- logging/log.go
+++ logging/log.go
@@ -3,6 +3,7 @@ package log
 import (
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/log"
+	"time"
 )
 
 func Debug(ctx context.Context, format string, args ...interface{}) {
@@ -27,4 +28,81 @@ func Errorf(ctx context.Context, format string, args ...interface{}) {
 // Criticalf is like Debugf, but at Critical level.
 func Criticalf(ctx context.Context, format string, args ...interface{}) {
 	internal.Logf(ctx, 4, format, args...)
+}
+
+func Logger() gin.HandlerFunc {
+	return func(c *Context) {
+		// Start timer
+		start := time.Now()
+		path := c.Request.URL.Path
+
+		// Process request
+		c.Next()
+
+		// Stop timer
+		end := time.Now()
+		latency := end.Sub(start)
+
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+		statusCode := c.Writer.Status()
+		statusColor := colorForStatus(statusCode)
+		methodColor := colorForMethod(method)
+		comment := c.Errors.ByType(ErrorTypePrivate).String()
+
+		Infof(c, "[GIN] %v |%s %3d %s| %13v | %s |%s  %s %-7s %s\n%s",
+			end.Format("2006/01/02 - 15:04:05"),
+			statusColor, statusCode, reset,
+			latency,
+			clientIP,
+			methodColor, reset, method,
+			path,
+			comment,
+		)
+	}
+}
+
+var (
+	green   = string([]byte{27, 91, 57, 55, 59, 52, 50, 109})
+	white   = string([]byte{27, 91, 57, 48, 59, 52, 55, 109})
+	yellow  = string([]byte{27, 91, 57, 55, 59, 52, 51, 109})
+	red     = string([]byte{27, 91, 57, 55, 59, 52, 49, 109})
+	blue    = string([]byte{27, 91, 57, 55, 59, 52, 52, 109})
+	magenta = string([]byte{27, 91, 57, 55, 59, 52, 53, 109})
+	cyan    = string([]byte{27, 91, 57, 55, 59, 52, 54, 109})
+	reset   = string([]byte{27, 91, 48, 109})
+)
+
+func colorForStatus(code int) string {
+	switch {
+	case code >= 200 && code < 300:
+		return green
+	case code >= 300 && code < 400:
+		return white
+	case code >= 400 && code < 500:
+		return yellow
+	default:
+		return red
+	}
+}
+
+func colorForMethod(method string) string {
+	switch method {
+	case "GET":
+		return blue
+	case "POST":
+		return cyan
+	case "PUT":
+		return yellow
+	case "DELETE":
+		return red
+	case "PATCH":
+		return green
+	case "HEAD":
+		return magenta
+	case "OPTIONS":
+		return white
+	default:
+		return reset
+	}
 }
